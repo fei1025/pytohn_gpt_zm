923a6b080f2d4da2c79e6be24e7f6bc88622b76f
zzf <damo0821@163.com>
2023-10-11 17:35:41 +0800

    添加demo

--- ui/test/widget_test.dart
+++ ui/test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:open_ui/main.dart';
+import 'package:open_ui/demo/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
