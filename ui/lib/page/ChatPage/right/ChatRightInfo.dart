b4b700c2a74048341cef0ce2359ba721518850e1
damo <damo0821@163.com>
2023-11-28 00:44:52 +0800

    修改md显示异常问题

--- ui/lib/page/ChatPage/right/ChatRightInfo.dart
+++ ui/lib/page/ChatPage/right/ChatRightInfo.dart
@@ -23,29 +23,61 @@ class ChatRightInfo extends StatefulWidget {
 class _ChatRightInfo extends State<ChatRightInfo> {
   @override
   Widget build(BuildContext context) {
-    var appState = context.read<MyAppState>();
-    List<String> msessage = [];
-    msessage.add("### 这是一个标题");
-    msessage.add("这是一个普通内容");
-    msessage.add(
-        ""
-            "这是一个普通内容"
-            "```   "
-            "class MarkdownHelper {Map<String, Widget> getTitleWidget(m.Node node) "
-            "=> title.getTitleWidget(node);Widget getPWidget(m.Element node) => p.getPWidget(node);"
-            "Widget getPreWidget(m.Node node) => pre.getPreWidget(node); }"
-            "```");
-    bool isDarkMode = appState.isDarkMode;
-    final config =
-    isDarkMode ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
-    codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
-    return ListView.builder(
-        itemCount: msessage.length,
-        itemBuilder: (context, index) {
-          return  ListTile(title:Container(child: Column(
-              children: MarkdownGenerator(
-                  ).buildWidgets(msessage[index]))));
+    var appState = context.watch<MyAppState>();
+    List msessage = [];
 
-        });
+    msessage.add({"user":"me","conter":"## 这是一个标题"});
+    msessage.add({"user":"me","conter":"#### 这是一个普通内容"});
+    msessage.add({"user":"you","conter":  ""
+        "这是一个普通内容"
+        "``` "
+        "class MarkdownHelper {Map<String, Widget> getTitleWidget(m.Node node) "
+        "=> title.getTitleWidget(node);Widget getPWidget(m.Element node) => p.getPWidget(node);"
+        "Widget getPreWidget(m.Node node) => pre.getPreWidget(node); }"
+        " ```"});
+
+    return Scaffold(
+      body: SelectionArea(
+          child: ListView.builder(
+              itemCount: msessage.length,
+              itemBuilder: (context, index) {
+                return ListTile(
+                  leading:msessage[index]["user"]=="me" ?CircleAvatar(child: Text("you"),) :CircleAvatar(child: Text("gpt"),),
+                    title: Container(
+                        constraints: BoxConstraints(
+                          maxWidth: MediaQuery.of(context).size.width *
+                              0.6, // 限制宽度
+                        ),
+                        alignment: msessage[index]["user"] == "me"
+                            ? Alignment.centerRight
+                            : Alignment.centerLeft,                        child: Column(
+                            children: getmd(context,msessage[index]["conter"]))
+
+
+                    ));
+              }),
+      ),
+    );
   }
 }
+
+
+List<Widget> getmd(BuildContext  context,String data){
+  var appState = context.watch<MyAppState>(); // 使用 context.watch 监听 MyAppsState 的变化
+  bool isDarkMode = appState.isDarkMode;
+  final config =
+  isDarkMode ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
+  codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
+  final config1 = config.copy(configs: [
+    isDarkMode? PreConfig.darkConfig.copy(wrapper: codeWrapper): PreConfig().copy(wrapper: codeWrapper)
+  ]);
+
+  return MarkdownGenerator(  config: config1).buildWidgets(data,
+  );
+}
+
+
+
+
+
+
--- ui/lib/page/md/MarkdownPage.dart
+++ ui/lib/page/md/MarkdownPage.dart
@@ -1,82 +1,81 @@
-import 'package:flutter/material.dart';
-import 'package:flutter/services.dart';
-import 'package:markdown_widget/config/configs.dart';
-import 'package:markdown_widget/config/markdown_generator.dart';
-import 'package:markdown_widget/config/toc.dart';
-import 'package:markdown_widget/widget/blocks/leaf/code_block.dart';
-import 'package:markdown_widget/widget/blocks/leaf/link.dart';
-import 'package:markdown_widget/widget/markdown.dart';
-import 'package:provider/provider.dart';
-
-import '../state.dart';
-import 'code_wrapper.dart';
-import 'markdown_custom/custom_node.dart';
-import 'markdown_custom/latex.dart';
-import 'markdown_custom/video.dart';
-
-class MarkdownPage extends StatelessWidget {
-  final String data;
-
-  MarkdownPage(this.data);
-
-  @override
-  Widget build(BuildContext context) {
-    var appState = context.watch<MyAppState>();
-    bool isDarkMode = appState.isDarkMode;
-    final config =
-        isDarkMode ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
-    codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
-    final config1 = config.copy(configs: [
-      LinkConfig(
-        style: TextStyle(
-          color: Colors.red,
-          decoration: TextDecoration.underline,
-        ),
-        onTap: (url) {
-          ///TODO:on tap
-        },
-      ),
-      isDarkMode
-          ? PreConfig.darkConfig.copy(wrapper: codeWrapper)
-          : PreConfig().copy(wrapper: codeWrapper)
-    ]);
-    // return Column(children: MarkdownGenerator().buildWidgets(data,config:config1
-    // ),);
-
-    return  MarkdownWidget(
-      data: data,
-      config: config.copy(configs: [
-        isDarkMode
-            ? PreConfig.darkConfig.copy(wrapper: codeWrapper)
-            : PreConfig().copy(wrapper: codeWrapper)
-      ]),
-      markdownGenerator: MarkdownGenerator(
-          generators: [videoGeneratorWithTag, latexGenerator],
-          inlineSyntaxList: [LatexSyntax()],
-          textGenerator: (node, config, visitor) =>
-              CustomTextNode(node.textContent, config, visitor)),
-    );
-  }
-
-  Widget buildMarkdown(BuildContext context) {
-    final TocController controller = TocController();
-    var appState = context.watch<MyAppState>();
-    bool isDarkMode = appState.isDarkMode;
-    final config =
-        isDarkMode ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
-    codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
-    return MarkdownWidget(
-        data: data,
-        config: config.copy(configs: [
-          isDarkMode
-              ? PreConfig.darkConfig.copy(wrapper: codeWrapper)
-              : PreConfig().copy(wrapper: codeWrapper)
-        ]),
-        tocController: controller,
-        markdownGenerator: MarkdownGenerator(
-            generators: [videoGeneratorWithTag, latexGenerator],
-            inlineSyntaxList: [LatexSyntax()],
-            textGenerator: (node, config, visitor) =>
-                CustomTextNode(node.textContent, config, visitor)));
-  }
-}
+// import 'package:flutter/material.dart';
+// import 'package:flutter/services.dart';
+// import 'package:markdown_widget/config/configs.dart';
+// import 'package:markdown_widget/config/markdown_generator.dart';
+// import 'package:markdown_widget/config/toc.dart';
+// import 'package:markdown_widget/widget/blocks/leaf/code_block.dart';
+// import 'package:markdown_widget/widget/blocks/leaf/link.dart';
+// import 'package:markdown_widget/widget/markdown.dart';
+// import 'package:provider/provider.dart';
+//
+// import '../state.dart';
+// import 'code_wrapper.dart';
+// import 'markdown_custom/custom_node.dart';
+// import 'markdown_custom/latex.dart';
+// import 'markdown_custom/video.dart';
+//
+// class MarkdownPage extends StatelessWidget {
+//   final String data;
+//
+//   MarkdownPage(this.data);
+//   @override
+//   Widget build(BuildContext context) {
+//     var appState = context.watch<MyAppState>();
+//     bool isDarkMode = appState.isDarkMode;
+//     final config =
+//         isDarkMode ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
+//     codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
+//     final config1 = config.copy(configs: [
+//       LinkConfig(
+//         style: TextStyle(
+//           color: Colors.red,
+//           decoration: TextDecoration.underline,
+//         ),
+//         onTap: (url) {
+//           ///TODO:on tap
+//         },
+//       ),
+//       isDarkMode
+//           ? PreConfig.darkConfig.copy(wrapper: codeWrapper)
+//           : PreConfig().copy(wrapper: codeWrapper)
+//     ]);
+//     // return Column(children: MarkdownGenerator().buildWidgets(data,config:config1
+//     // ),);
+//
+//     return  MarkdownWidget(
+//       data: data,
+//       config: config.copy(configs: [
+//         isDarkMode
+//             ? PreConfig.darkConfig.copy(wrapper: codeWrapper)
+//             : PreConfig().copy(wrapper: codeWrapper)
+//       ]),
+//       markdownGenerator: MarkdownGenerator(
+//           generators: [videoGeneratorWithTag, latexGenerator],
+//           inlineSyntaxList: [LatexSyntax()],
+//           textGenerator: (node, config, visitor) =>
+//               CustomTextNode(node.textContent, config, visitor)),
+//     );
+//   }
+//
+//   Widget buildMarkdown(BuildContext context) {
+//     final TocController controller = TocController();
+//     var appState = context.watch<MyAppState>();
+//     bool isDarkMode = appState.isDarkMode;
+//     final config =
+//         isDarkMode ? MarkdownConfig.darkConfig : MarkdownConfig.defaultConfig;
+//     codeWrapper(child, text) => CodeWrapperWidget(child: child, text: text);
+//     return MarkdownWidget(
+//         data: data,
+//         config: config.copy(configs: [
+//           isDarkMode
+//               ? PreConfig.darkConfig.copy(wrapper: codeWrapper)
+//               : PreConfig().copy(wrapper: codeWrapper)
+//         ]),
+//         tocController: controller,
+//         markdownGenerator: MarkdownGenerator(
+//             generators: [videoGeneratorWithTag, latexGenerator],
+//             inlineSyntaxList: [LatexSyntax()],
+//             textGenerator: (node, config, visitor) =>
+//                 CustomTextNode(node.textContent, config, visitor)));
+//   }
+// }
