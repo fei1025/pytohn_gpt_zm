b2123ba4c46715112eba12b63754ad15e4d89d08
damo <damo0821@163.com>
2024-01-21 21:58:53 +0800

    添加数据

---
+++ ui/lib/page/knowledge/right/KnowledgeRightInfo.dart
@@ -0,0 +1,15 @@
+import 'package:flutter/cupertino.dart';
+
+class KnowledgeRightInfo extends StatefulWidget {
+  const KnowledgeRightInfo({super.key});
+
+  @override
+  State<KnowledgeRightInfo> createState() => _KnowledgeRightInfoState();
+}
+
+class _KnowledgeRightInfoState extends State<KnowledgeRightInfo> {
+  @override
+  Widget build(BuildContext context) {
+    return const Placeholder();
+  }
+}
